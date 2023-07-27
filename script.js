68c6bb672434d07897573dc0f1430dfaa8a8aaea
Shweta Sidhwani <121174976+purplefish15@users.noreply.github.com>
2023-07-27 19:20:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var img = document.getElementById('img');
-var slides = ['GreenDress.jpg','PinkDress.jpg','RedSweater.jpg','WhiteSweater.jpg','CoatEnsemble.jpg'];
+var slides = ['https://cdn.forcast.com.au/media/webp_image/wysiwyg/JUL5_MAIN_MOBILE.webp3','https://i.pinimg.com/originals/b6/a8/42/b6a8429f8097c52d13081dece51b544a.jpg','https://i5.walmartimages.com/asr/49449dc5-f0d7-4bb2-9664-77072f33fcc9.0d62633a014df1006e4d5b6830005204.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF','https://as2.ftcdn.net/v2/jpg/04/84/64/31/1000_F_484643174_Tal7FyQ9fuVJogXBGutDim6BEUZRdCgm.jpg','https://cdn.forcast.com.au/media/webp_image/wysiwyg/JUL5_MAIN_MOBILE.webp'];
+
 var Start=0;
 function slider(){
     if(Start<slides.length){
